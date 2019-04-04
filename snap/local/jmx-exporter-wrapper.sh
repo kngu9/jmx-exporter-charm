7b61638815f6eec49a3b500e094c49f036346292
kngu9 <khanhtnguyen300@gmail.com>
2019-04-04 15:36:59 +0000

    Fixed config

--- snap/local/jmx-exporter-wrapper.sh
+++ snap/local/jmx-exporter-wrapper.sh
@@ -7,4 +7,4 @@ if [ ! -f $SNAP_COMMON/config.yaml ]; then
 	exit 1
 fi
 
-java -jar $SNAP/jmx_prometheus_httpserver/jmx_prometheus_httpserver-0.11.0-jar-with-dependencies.jar $SNAP_COMMON/config.yaml
+java -jar $SNAP/jmx_prometheus_httpserver/jmx_prometheus_httpserver-0.11.0-jar-with-dependencies.jar 0.0.0.0:4081 $SNAP_COMMON/config.yaml
